0a7b214a9d0aebd67e153ed6ee1792e3dd10e797
Charles Dunn <dunnc1551@gmail.com>
2017-03-28 17:26:15 -0600

    Add App directory and install inital npm modules

--- app/index.js
+++ app/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from './components/App/App';
 import MovieIndex from './components/movieIndex';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import { Router, Route, IndexRoute } from 'react-router';
+import createHistory from 'history/createBrowserHistory';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+import { Provider } from 'react-redux';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const history = createHistory();
+const middleWare = routerMiddleware(history);
+
+const rootReducer = combineReducers({
+  router: routerReducer
+});
+
+const store = createStore(rootReducer, devTools, applyMiddleware(middleWare));
 
 const router = (
-    <Router history={browserHistory} >
+  <Provider store={store}>
+    <ConnectedRouter history={history} >
       <Route path='/' component={App}>
         <IndexRoute component={MovieIndex} />
       </Route>
-    </Router>
+    </ConnectedRouter>
+  </Provider>
 )
 
 ReactDOM.render(router, document.getElementById('main'))
